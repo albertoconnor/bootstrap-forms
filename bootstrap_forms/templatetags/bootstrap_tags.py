2372b53cccf709498dbb48f3849521eebdcea3e7
Albert O'Connor <amjoconn@gmail.com>
2014-08-19 13:59:09 -0400

    Update as bootstrap to include the widget class.

--- bootstrap_forms/templatetags/bootstrap_tags.py
+++ bootstrap_forms/templatetags/bootstrap_tags.py
@@ -20,17 +20,30 @@ default_field_template = '''<div class="form-group">
 </div>
 '''
 
+#TODO make placeholder tag!
+@register.filter()
+def placeholder(field, placeholder_text):
+    field.field.widget.attrs['placeholder'] = placeholder_text
+    return field
+
 @register.filter(is_safe=True)
 def as_bootstrap(field):
+    if ('class' not in field.field.widget.attrs
+        or 'form-control' not in field.field.widget.attrs['class']):
+
+        add_class(field.field, 'form-control')
+
+    #if 'placeholder' not in field.field.widget.attrs:
+    #    field.field.widget.attrs['placeholder'] = unicode(field.label)
+
     template = Template(default_field_template)
     ctx = Context(dict(field=field))
     return template.render(ctx)
 
-@register.filter()
 def add_class(field, class_):
-    classes = ['class_']
+    classes = [class_]
     if 'class' in field.widget.attrs:
-        classes.append(field.widget.attrs['class'])
+        classes.append(field.widget.attrs['class'].split(' '))
     field.widget.attrs.update(
         {'class' : ' '.join(classes)}
     )
