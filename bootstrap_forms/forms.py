8c1450fa04172cc4bd0dab928182f0fa47b4e3e4
Albert O'Connor <amjoconn@gmail.com>
2018-08-08 19:21:59 -0400

    Remove special radio render since how this is supported changes in different versions of django.

--- bootstrap_forms/forms.py
+++ bootstrap_forms/forms.py
@@ -2,14 +2,6 @@ import types
 
 from django import forms
 from django.template import Template, Context
-from django.utils.html import format_html, format_html_join
-from django.utils.encoding import force_text, python_2_unicode_compatible
-
-try:
-    from django.forms.widgets import RadioSelect
-except ImportError:
-    from django.forms.widgets import RadioInput
-    RadioSelect = RadioInput
 
 
 default_template = u'''
@@ -57,10 +49,12 @@ default_field_template = u'''<div class="form-group">
 </div>
 '''
 
+
 def as_bootstrap(self):
     template = Template(default_field_template)
     ctx = Context(dict(field=self))
     return template.render(ctx)
+
 
 # Mixin
 class BootstrapForm(object):
@@ -68,63 +62,25 @@ class BootstrapForm(object):
         super(BootstrapForm, self).__init__(*args, **kwargs)
         for bfield in self:
             # Bind a helper method to each field to render as bootstrap
-            bfield.as_bootstrap =  types.MethodType(as_bootstrap, bfield)
+            bfield.as_bootstrap = types.MethodType(as_bootstrap, bfield)
 
             field = self.fields[bfield.name]
             to_continue = False
-            for widget_class in (forms.CheckboxSelectMultiple, forms.CheckboxInput, BootStrapRadioSelect):
+            for widget_class in (forms.CheckboxSelectMultiple, forms.CheckboxInput):
                 if isinstance(field.widget, widget_class):
                     to_continue = True
                     break
-            if to_continue: continue
+            if to_continue:
+                continue
 
             classes = ['form-control']
             if 'class' in field.widget.attrs:
                 classes.append(field.widget.attrs['class'])
             field.widget.attrs.update(
-                {'class' : ' '.join(classes)}
+                {'class': ' '.join(classes)}
             )
 
     def as_bootstrap(self):
         template = Template(default_template)
         ctx = Context(dict(form=self))
         return template.render(ctx)
-
-class BootstrapRadioInput(RadioSelect):
-    def render(self, name=None, value=None, attrs=None, choices=()):
-        name = name or self.name
-        value = value or self.value
-        attrs = attrs or self.attrs
-        if 'id' in self.attrs:
-            label_for = format_html(u' for="{0}_{1}"', self.attrs['id'], self.index)
-        else:
-            label_for = u''
-        choice_label = force_text(self.choice_label)
-        return format_html(
-            u'<label{0} class="radio inline">{1} {2}</label>',
-            label_for,
-            self.tag(),
-            self.choice_label
-        )
-
-
-@python_2_unicode_compatible
-class BootStrapRadioRenderer(forms.widgets.RadioFieldRenderer):
-    def __iter__(self):
-        for i, choice in enumerate(self.choices):
-            yield BootstrapRadioInput(self.name, self.value, self.attrs.copy(), choice, i)
-
-    def render(self):
-        return format_html(
-            u'{0}',
-            format_html_join(u'\n', u'{0}', [(force_text(w),) for w in self])
-        )
-
-    def __str__(self):
-        return b''
-
-
-class BootStrapRadioSelect(forms.RadioSelect):
-    renderer = BootStrapRadioRenderer
-    inline = True
-
